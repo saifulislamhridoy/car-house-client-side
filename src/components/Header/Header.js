2f377a57377df128a14188bf659ee1871c590745
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-05-07 16:51:39 +0600

    added banner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ import auth from '../../firebase.init';
 const Header = () => {
     const [user, loading, error] = useAuthState(auth);
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand as={Link} to="/"><span className='text-danger'>Car</span> House</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
