174505aec91ebe4df2580aaab75af241db9b5f0c
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-05-09 00:22:38 +0600

    added deliver part

--- src/components/UpdateProduct/UpdateProduct.js
+++ src/components/UpdateProduct/UpdateProduct.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import useProductDetail from '../../hooks/useProductDetail';
 import './UpdateProduct.css'
 
 const UpdateProduct = () => {
-    const [product] = useProductDetail()
-    const {_id,img,itemName,price,quantity,description,supplierName}=product
-    const handleDeliver = (product,id)=>{
+    const {productId}=useParams()
+    const [product,setProduct] = useProductDetail()
+    const {img,itemName,price,quantity,description,supplierName}=product
+    const handleDeliver = (product)=>{
     const newQuantity = parseInt(product.quantity)-1
-    console.log(newQuantity)
+    fetch(`http://localhost:5000/product/${productId}`,{
+        method:'PUT',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify({newQuantity})
+    })
+    .then(res=>res.json())
+    .then(result=> {
+        console.log(result)
+        setProduct({...product,quantity:newQuantity})
+        toast('Success Delivered')
+    })
     }
     return (
         <div>
@@ -25,7 +40,7 @@ const UpdateProduct = () => {
                             <p className="card-text">{description?.slice(0,80)}</p>
                             <input className='inp-field' type="number" name="" id="" placeholder='Quantity'/>
                             <button className='btn bg-primary text-white'>Restock</button>
-                            <button onClick={()=>handleDeliver(product,_id)} className='mt-4 d-block mx-auto btn bg-danger text-white'>Deliver</button>
+                            <button onClick={()=>handleDeliver(product)} className='mt-4 d-block mx-auto btn bg-danger text-white'>Deliver</button>
                         </div>
                     </div>
                 </div>
--- src/hooks/useProductDetail.js
+++ src/hooks/useProductDetail.js
@@ -9,6 +9,6 @@ const useProductDetail = () =>{
         .then(res => res.json())
         .then(data => setProduct(data))
     },[])
-    return [product]
+    return [product,setProduct]
 }
 export default useProductDetail;
